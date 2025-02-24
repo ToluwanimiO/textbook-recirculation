b39015f9bf6c28f99f1b95d85c598ffd13f485b7
ToluwanimiO <toluwanimiosuolale@gmail.com>
2025-02-24 14:37:06 +0100

    fix error

--- routes/utilityRoutes.js
+++ routes/utilityRoutes.js
@@ -3,6 +3,6 @@ const { getCountries, getStates } = require("../controllers/utilityController");
 
 const router = express.Router();
 router.get("/countries", getCountries);
-router.get("states/:countryCode", getStates);
+router.get("/states/:countryCode", getStates);
 
 module.exports = router;
